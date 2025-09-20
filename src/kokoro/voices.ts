e2e6e74af996de446833eee1b37a1c22da230ab2
danemadsen <dane_madsen@hotmail.com>
2025-09-20 18:55:43 +1000

    fix voices

--- src/kokoro/voices.ts
+++ src/kokoro/voices.ts
@@ -76,74 +76,74 @@ export type KokoroVoice = typeof KokoroVoices[number];
 
 const VOICE_ASSETS: Record<KokoroVoice, number> = {
   // American Female
-  alloy: require("../../assets/tts/voices/af_alloy.bin"),
-  aoede: require("../../assets/tts/voices/af_aoede.bin"),
-  bella: require("../../assets/tts/voices/af_bella.bin"),
-  heart: require("../../assets/tts/voices/af_heart.bin"),
-  jessica: require("../../assets/tts/voices/af_jessica.bin"),
-  kore: require("../../assets/tts/voices/af_kore.bin"),
-  nicole: require("../../assets/tts/voices/af_nicole.bin"),
-  nova: require("../../assets/tts/voices/af_nova.bin"),
-  river: require("../../assets/tts/voices/af_river.bin"),
-  sarah: require("../../assets/tts/voices/af_sarah.bin"),
-  sky: require("../../assets/tts/voices/af_sky.bin"),
+  alloy: require("../../assets/voices/af_alloy.bin"),
+  aoede: require("../../assets/voices/af_aoede.bin"),
+  bella: require("../../assets/voices/af_bella.bin"),
+  heart: require("../../assets/voices/af_heart.bin"),
+  jessica: require("../../assets/voices/af_jessica.bin"),
+  kore: require("../../assets/voices/af_kore.bin"),
+  nicole: require("../../assets/voices/af_nicole.bin"),
+  nova: require("../../assets/voices/af_nova.bin"),
+  river: require("../../assets/voices/af_river.bin"),
+  sarah: require("../../assets/voices/af_sarah.bin"),
+  sky: require("../../assets/voices/af_sky.bin"),
 
   // American Male
-  adam: require("../../assets/tts/voices/am_adam.bin"),
-  echo: require("../../assets/tts/voices/am_echo.bin"),
-  eric: require("../../assets/tts/voices/am_eric.bin"),
-  fenrir: require("../../assets/tts/voices/am_fenrir.bin"),
-  liam: require("../../assets/tts/voices/am_liam.bin"),
-  michael: require("../../assets/tts/voices/am_michael.bin"),
-  onyx: require("../../assets/tts/voices/am_onyx.bin"),
-  puck: require("../../assets/tts/voices/am_puck.bin"),
-  santa: require("../../assets/tts/voices/am_santa.bin"),
+  adam: require("../../assets/voices/am_adam.bin"),
+  echo: require("../../assets/voices/am_echo.bin"),
+  eric: require("../../assets/voices/am_eric.bin"),
+  fenrir: require("../../assets/voices/am_fenrir.bin"),
+  liam: require("../../assets/voices/am_liam.bin"),
+  michael: require("../../assets/voices/am_michael.bin"),
+  onyx: require("../../assets/voices/am_onyx.bin"),
+  puck: require("../../assets/voices/am_puck.bin"),
+  santa: require("../../assets/voices/am_santa.bin"),
 
   // British Female
-  alice: require("../../assets/tts/voices/bf_alice.bin"),
-  emma: require("../../assets/tts/voices/bf_emma.bin"),
-  isabella: require("../../assets/tts/voices/bf_isabella.bin"),
-  lily: require("../../assets/tts/voices/bf_lily.bin"),
+  alice: require("../../assets/voices/bf_alice.bin"),
+  emma: require("../../assets/voices/bf_emma.bin"),
+  isabella: require("../../assets/voices/bf_isabella.bin"),
+  lily: require("../../assets/voices/bf_lily.bin"),
 
   // British Male
-  daniel: require("../../assets/tts/voices/bm_daniel.bin"),
-  fable: require("../../assets/tts/voices/bm_fable.bin"),
-  george: require("../../assets/tts/voices/bm_george.bin"),
-  lewis: require("../../assets/tts/voices/bm_lewis.bin"),
+  daniel: require("../../assets/voices/bm_daniel.bin"),
+  fable: require("../../assets/voices/bm_fable.bin"),
+  george: require("../../assets/voices/bm_george.bin"),
+  lewis: require("../../assets/voices/bm_lewis.bin"),
 
   // Foreign Female
-  siwis: require("../../assets/tts/voices/ff_siwis.bin"),
+  siwis: require("../../assets/voices/ff_siwis.bin"),
 
   // Hybrid freak show
-  alpha: require("../../assets/tts/voices/hf_alpha.bin"),
-  beta: require("../../assets/tts/voices/hf_beta.bin"),
-  omega: require("../../assets/tts/voices/hm_omega.bin"),
-  psi: require("../../assets/tts/voices/hm_psi.bin"),
+  alpha: require("../../assets/voices/hf_alpha.bin"),
+  beta: require("../../assets/voices/hf_beta.bin"),
+  omega: require("../../assets/voices/hm_omega.bin"),
+  psi: require("../../assets/voices/hm_psi.bin"),
 
   // Italian
-  sara: require("../../assets/tts/voices/if_sara.bin"),
-  nicola: require("../../assets/tts/voices/im_nicola.bin"),
+  sara: require("../../assets/voices/if_sara.bin"),
+  nicola: require("../../assets/voices/im_nicola.bin"),
 
   // Japanese
-  jf_alpha: require("../../assets/tts/voices/jf_alpha.bin"),
-  gongitsune: require("../../assets/tts/voices/jf_gongitsune.bin"),
-  nezumi: require("../../assets/tts/voices/jf_nezumi.bin"),
-  tebukuro: require("../../assets/tts/voices/jf_tebukuro.bin"),
-  kumo: require("../../assets/tts/voices/jm_kumo.bin"),
+  jf_alpha: require("../../assets/voices/jf_alpha.bin"),
+  gongitsune: require("../../assets/voices/jf_gongitsune.bin"),
+  nezumi: require("../../assets/voices/jf_nezumi.bin"),
+  tebukuro: require("../../assets/voices/jf_tebukuro.bin"),
+  kumo: require("../../assets/voices/jm_kumo.bin"),
 
   // Portuguese
-  dora: require("../../assets/tts/voices/pf_dora.bin"),
-  alex: require("../../assets/tts/voices/pm_alex.bin"),
+  dora: require("../../assets/voices/pf_dora.bin"),
+  alex: require("../../assets/voices/pm_alex.bin"),
 
   // Chinese
-  xiaobei: require("../../assets/tts/voices/zf_xiaobei.bin"),
-  xiaoni: require("../../assets/tts/voices/zf_xiaoni.bin"),
-  xiaoxiao: require("../../assets/tts/voices/zf_xiaoxiao.bin"),
-  xiaoyi: require("../../assets/tts/voices/zf_xiaoyi.bin"),
-  yunjian: require("../../assets/tts/voices/zm_yunjian.bin"),
-  yunxi: require("../../assets/tts/voices/zm_yunxi.bin"),
-  yunxia: require("../../assets/tts/voices/zm_yunxia.bin"),
-  yunyang: require("../../assets/tts/voices/zm_yunyang.bin"),
+  xiaobei: require("../../assets/voices/zf_xiaobei.bin"),
+  xiaoni: require("../../assets/voices/zf_xiaoni.bin"),
+  xiaoxiao: require("../../assets/voices/zf_xiaoxiao.bin"),
+  xiaoyi: require("../../assets/voices/zf_xiaoyi.bin"),
+  yunjian: require("../../assets/voices/zm_yunjian.bin"),
+  yunxi: require("../../assets/voices/zm_yunxi.bin"),
+  yunxia: require("../../assets/voices/zm_yunxia.bin"),
+  yunyang: require("../../assets/voices/zm_yunyang.bin"),
 };
 
 export async function load_voice_data(voice: KokoroVoice): Promise<Float32Array> {
