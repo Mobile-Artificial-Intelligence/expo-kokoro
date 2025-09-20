239b9ad787499a9b55e47ec27319586801658515
danemadsen <dane_madsen@hotmail.com>
2025-09-20 18:46:56 +1000

    export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Kokoro } from "./kokoro/kokoro";
 export { Vits } from "./vits/vits";
-export { KokoroVoice } from "./kokoro/voices";
+export { KokoroVoice, KokoroVoices } from "./kokoro/voices";
 export { DeepPhonemizer } from "./deep-phonemizer/deep-phonemizer";
